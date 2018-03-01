873a0bb6070d7eab60cf368df964bba8f2693883
Omar Cesar <omar-nav@users.noreply.github.com>
2018-03-01 12:20:41 -0600

    Access HTML element information within JS

---
+++ tt.js
@@ -0,0 +1,28 @@
+/*JavaScript 6th Edition
+* Chapter 3
+* Chapter case Tipton case
+* Tipton turbines
+* Variables and functions
+* Filename: tt.js
+*/
+// global variables
+// lenght counts from one
+// element indices start at 0
+var daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+var opponents= ["Lightning","Combines","Combines","Combines",
+"Lightning","Lightning","Lightning","Lightning",
+"Barn Raisers","Barn Raisers","Barn Raisers",
+"Sodbusters","Sodbusters","Sodbusters",
+"Sodbusters","(off)","River Riders",
+"River Riders","River Riders","Big Dippers",
+"Big Dippers","Big Dippers","(off)",
+"Sodbusters","Sodbusters","Sodbusters",
+"Combines","Combines","Combines","(off)",
+"(off)"];
+
+var gameLocation = ["away", "away", "away", "away","home", "home",
+"home", "home", "home", "home", "home", "away",
+"away", "away", "away", "", "away", "away", "away",
+"away", "away", "away", "", "home", "home", "home",
+"home", "home", "home", "", ""];
